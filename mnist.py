373e800ddb72ca5b90bba50b49415eec8dd26e64
Adnan Hossain <adnanhossain274@gmail.com>
2020-05-14 17:02:47 +0600

    Update mnist.py

--- mnist.py
+++ mnist.py
@@ -5,10 +5,12 @@ np.random.seed(1)
 
 from keras.datasets import mnist
 
+#initializing training and testing dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 images, labels = (x_train[0:1000].reshape(1000, 28 * 28) / 255, y_train[0:1000])
 
+#one hot encoding used as output can be any value from 1-10
 one_hot_labels = np.zeros((len(labels), 10))
 for i, l in enumerate(labels):
     one_hot_labels[i][l] = 1
@@ -20,6 +22,7 @@ for i, l in enumerate(y_test):
     test_labels[i][l] = 1
 
 
+#activation function for the hidden-layer    
 def tanh(x):
     return np.tanh(x)
 
@@ -28,15 +31,20 @@ def tanh2deriv(output):
     return 1 - (output ** 2)
 
 
+#activation function for the output layer
 def softmax(x):
     temp = np.exp(x)
     return temp / np.sum(temp, axis=1, keepdims=True)
 
 
+#for mini-batch gradient descent
 batch_size = 100
+
+#alpha --> step size
 alpha, iterations = (2, 300)
 hidden_size, pixels_per_image, num_labels = (100, 784, 10)
 
+#random initialization of weight matrices 
 weights_0_1 = 0.02 * np.random.random((pixels_per_image, hidden_size)) - 0.01
 weights_1_2 = 0.2 * np.random.random((hidden_size, num_labels)) - 0.1
 
@@ -48,18 +56,24 @@ for j in range(iterations):
 
         layer_0 = images[batch_start:batch_end]
         layer_1 = tanh(np.dot(layer_0, weights_0_1))
+        
+        # dropout used to turn random neurons off
         dropout_mask = np.random.randint(2, size=layer_1.shape)
+        
+        # dropout implemented to the hidden layer
         layer_1 *= dropout_mask * 2
         layer_2 = softmax(np.dot(layer_1, weights_1_2))
 
         for k in range(batch_size):
             correct_cnt += int(np.argmax(layer_2[k:k + 1]) ==
                                np.argmax(labels[batch_start + k:batch_end + k]))
-
+        
+        # backward propagation for assigning delta values to the hidden layers
         layer_2_delta = (labels[batch_start:batch_end] - layer_2) / (batch_size * layer_2.shape[0])
         layer_1_delta = layer_2_delta.dot(weights_1_2.T) * tanh2deriv(layer_1)
         layer_1_delta *= dropout_mask
-
+        
+        #updating weights accordingly
         weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
         weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
 
